edec0385a2c6886a47b475bb3f363f9c7cf3c7ec
Oleg Braginskyy <obr@sitecore.net>
2017-01-10 13:14:51 +0200

    Commit

---
+++ src/Sitecore.Support.91708/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.91708")]
+[assembly: AssemblyProduct("Sitecore.Support.91708")]
+[assembly: ComVisible(false)]
